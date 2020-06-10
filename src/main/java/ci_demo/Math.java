a09e07529368cf1ad7527b2f9fd3afedcea63688
Mirta Arambulo <arambulomirta@gmail.com>
2020-06-10 08:43:50 -0400

    First version of the project

---
+++ src/main/java/ci_demo/Math.java
@@ -0,0 +1,14 @@
+package ci_demo;
+
+public class Math {
+	
+	/***
+	 * Sum the numbers
+	 * @param a
+	 * @param b
+	 * @return the sum as integer
+	 */
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/test/java/ci_demo/MathTest.java
@@ -0,0 +1,14 @@
+package ci_demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+
+	@Test
+	void testSum() {
+		assertEquals(2, Math.sum(1,1));
+	}
+
+}
